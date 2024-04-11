2e22b0c80d036c9576fbb0401a31c284369fa86e
LlucGuardiola <arceus88205@gmail.com>
2024-04-11 16:33:56 +0200

    Plataforma axtualitzada

--- Assets/Scripts/MovePlatform.cs
+++ Assets/Scripts/MovePlatform.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class MovePlatform : MonoBehaviour
 {
+    private GameObject gameObj; 
     [SerializeField] private float speed;
     [SerializeField] private float unitsToRight;
+    [SerializeField] private bool startsMovingRight;
+    [SerializeField] private bool canMove;
 
     private Vector3 iniPosition;
-    private bool movingRight;
-    private bool canMove;
 
     // Start is called before the first frame update
     void Start()
     {
-        movingRight = true;
+        gameObj = this.GameObject();
         iniPosition = transform.position;
         canMove = true;
+        gameObj.AddComponent<Rigidbody2D>().GetComponent<Rigidbody2D>().gravityScale = 0;
+        gameObj.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
     }
 
     // Update is called once per frame
@@ -30,18 +35,18 @@ public class MovePlatform : MonoBehaviour
     }
     private void Move()
     {
-        if (movingRight) { gameObject.transform.Translate(Mathf.Abs(speed) * Time.deltaTime, 0, 0) ; }
+        if (startsMovingRight) { gameObject.transform.Translate(Mathf.Abs(speed) * Time.deltaTime, 0, 0) ; }
         else { gameObject.transform.Translate(-Mathf.Abs(speed) * Time.deltaTime, 0, 0); }
 
         if (transform.position.x >= iniPosition.x + unitsToRight || transform.position.x <= iniPosition.x)
         {
-            movingRight = !movingRight;
+            startsMovingRight = !startsMovingRight;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         canMove = false;
-    }
+    } 
     private void OnCollisionExit2D(Collision2D collision)
     {
         canMove = true;
