4d214fa7de49ed4572a2d18d002f344ac0157f9f
LlucGuardiola <arceus88205@gmail.com>
2024-04-17 16:29:43 +0200

    s

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -160,7 +160,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.Alpha2))
         {
             speed = 1000000;
-            body.transform.position = new Vector3(261f, 2f, 0f);
+            body.transform.position = new Vector3(261f, 15f, 0f);
         }
         if (Input.GetKey(KeyCode.Alpha3))
         {
