a0c4cdf8eca5f8b19743cf1973d1534895b19991
LlucGuardiola <arceus88205@gmail.com>
2024-04-30 18:18:38 +0200

    Plantilla bosses feta

---
+++ Assets/Scripts/Enemies/BossEdgar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEdgar : Boss
+{
+    // Espai per crear variables
+
+    public BossEdgar(float vida, float damage) : base(vida, damage)
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override float GetDamage() { return damage; }
+    public override void MakeDamage(float damage) { vida -= damage; }
+    public override float GetVida() { return vida; }
+    public override void SetVida(float vida) { this.vida = vida; }
+}
