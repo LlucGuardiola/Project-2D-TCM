7969da9822fdc101894addb22eeae20d3e48d9f8
LlucGuardiola <arceus88205@gmail.com>
2024-05-21 18:15:54 +0200

    different slime colors

--- Assets/Enemies/Enemigo Slime.cs
+++ Assets/Enemies/Enemigo Slime.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class EnemigoSlime : MonoBehaviour
 {
+    [SerializeField] private bool RedSlime;
+    [SerializeField] private bool GreenSlime;
+    [SerializeField] private bool GraySlime;
+    [SerializeField] private bool BlueSlime;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("RedSlime", RedSlime);
+        animator.SetBool("GreenSlime", GreenSlime);
+        animator.SetBool("GraySlime", GraySlime);
+        animator.SetBool("BlueSlime", BlueSlime);
+    }
     public void OnCollisionEnter2D(Collision2D collision)
     {
          if (collision.gameObject.CompareTag("Player"))
