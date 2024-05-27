48da2eaf72491c48ace3d0a4d3aa3c27a0d24321
aimarsito <agomezi@edu.tecnocampus.cat>
2024-05-27 22:31:52 +0200

    cossas aimar

---
+++ Assets/PlataformaAimar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaAimar : MonoBehaviour
+{
+    [SerializeField] private Transform[] puntosMovimiento;
+    [SerializeField] private float velocidad;
+
+    private int siguientePlataforma = 1;
+    private bool ordenPlataformas = true;
+
+    private void Update()
+    {
+         
+
+
+    }
+}
