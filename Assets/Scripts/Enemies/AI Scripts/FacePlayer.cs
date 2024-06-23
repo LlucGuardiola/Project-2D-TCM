3544971ca499d9d3f2d005381e636765596a162a
jbatllec <janbatlle@gmail.com>
2024-06-23 23:24:02 +0200

    C

--- Assets/Scripts/Enemies/AI Scripts/FacePlayer.cs
+++ Assets/Scripts/Enemies/AI Scripts/FacePlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flip : MonoBehaviour
+public class FacePlayer : MonoBehaviour
 {
     // Start is called before the first frame update
    private Transform player;
